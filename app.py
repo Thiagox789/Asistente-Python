8fa53137801fc20098a4eddbc1b2aed267f663b8
Thiagox <almadathiagoignacio@gmail.com>
2024-10-20 10:15:10 -0300

    Primeros pasos

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-import speech_recognition
+import speech_recognition  
 import pyttsx3
 import subprocess
 import webbrowser
+
+reconocedor = speech_recognition.Recognizer()
+voz = pyttsx3.init()
+
+voz.setProperty('rate', 150)  # Velocidad de habla
+voz.setProperty('volume', 1)  # Volumen (entre 0.0 y 1.0)
+
+# Ejemplo de hablar un mensaje
+voz.say("Hola, ¿cómo estás?")
+voz.runAndWait()
