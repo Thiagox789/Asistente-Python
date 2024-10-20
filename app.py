64e6c931e75ab8a7cd0e418869c0f26437512ab8
Thiagox <almadathiagoignacio@gmail.com>
2024-10-20 10:19:54 -0300

    Anda

--- app.py
+++ app.py
@@ -3,12 +3,45 @@ import pyttsx3
 import subprocess
 import webbrowser
 
-reconocedor = speech_recognition.Recognizer()
+#Variables#
+mic_id = 1
 voz = pyttsx3.init()
+reconocedor = speech_recognition.Recognizer()
 
 voz.setProperty('rate', 150)  # Velocidad de habla
 voz.setProperty('volume', 1)  # Volumen (entre 0.0 y 1.0)
 
+
+#Ver mic
+#for index, name in enumerate(speech_recognition.Microphone.list_microphone_names()):
+    #print(f"Micrófono disponible: {index}: {name}")
+
+
 # Ejemplo de hablar un mensaje
-voz.say("Hola, ¿cómo estás?")
-voz.runAndWait()
+#voz.say("asdf")
+#voz.runAndWait()
+
+while True:
+    with speech_recognition.Microphone(device_index=mic_id) as fuente:
+        print("Calibrando el micrófono... por favor guarda silencio.")
+        reconocedor.adjust_for_ambient_noise(fuente)  # Para calibrar en ambientes ruidosos
+        print("Escuchando...")
+        audio = reconocedor.listen(fuente)
+        try:
+            # Reconocer la voz utilizando Google
+            texto = reconocedor.recognize_google(audio, language='es-ES')
+            print(f"Has dicho: {texto}")
+
+            # Reproducir el texto reconocido usando pyttsx3
+            voz.say(f"Has dicho: {texto}")
+            voz.runAndWait()
+
+        except speech_recognition.UnknownValueError:
+            print("No he entendido lo que dijiste.")
+            voz.say("No he entendido lo que dijiste.")
+            voz.runAndWait()
+
+        except speech_recognition.RequestError:
+            print("No se pudo conectar al servicio de reconocimiento.")
+            voz.say("No se pudo conectar al servicio de reconocimiento.")
+            voz.runAndWait()
